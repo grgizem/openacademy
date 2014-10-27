45d61a99eb4cda88a8df797afae8faf58751611b
Gizem Gur <gizem.gur@metglobal.com>
2014-10-27 10:25:54 +0200

    active session default field added

--- wizard/create_attendee.py
+++ wizard/create_attendee.py
@@ -6,6 +6,14 @@ class CreateAttendeeWizard(osv.TransientModel):
 	_columns = {'session_id': fields.many2one('openacademy.session', "Session", required=True),
 	            'attendee_ids': fields.many2many('res.partner', required=True, domain=[('instructor', '=', False)])}
 
+	def _get_active_session(self, cr, uid, context):
+		if context.get('active_model') == 'openacademy.session':
+			return context.get('active_id', False)
+		return False
+
+	_defaults = {'session_id': _get_active_session,}
+
+
 	def action_add_attendees(self, cr, uid, ids, context=None):
 		attendee_model = self.pool.get('openacademy.attendee')
 		wizard = self.browse(cr, uid, ids[0], context=context)
